be2b3568b01163cbc6b9005fd76e7100958d33ea
Andrew Yehozha <andrew.yehozha@tesseris.com>
2021-10-29 15:50:13 +0300

    Add project files.

---
+++ CiCDTestWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CiCDTestWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
